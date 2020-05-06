adfc728927612572a305be54a8d8cd6206eefdeb
Tharun kumar Doppalapudi <30587325+dt615k@users.noreply.github.com>
2020-05-06 11:18:43 -0500

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,52 @@
+var categories = {
+    "categories": [{
+        "name": "eat",
+        "values": ["bread", "cake", "fruit", "biscuits", "chicken", "veggies", "fruits", "chocolates","icecream","chips"]
+    },
+        {
+            "name": "yoga",
+            "values": ["handstand", "balasana", "bakasana", "kapotasana", "sirsasana", "tadasana", "vriksasana", "vajrasana","trikonasana","shavasana"]
+        },
+        {
+            "name": "drink",
+            "values": ["mojito", "water", "soda", "juice", "milkshake", "cocktail", "wine", "coconut water", "coffee", "lemonade", "tea"]
+        },
+        {
+            "name": "cooking",
+            "values": ["baking", "frying", "boiling \n vegetables", "mixing \n dough", "salad", "pasta", "noodles", "rice", "beans", "salsa", "dessert"
+            ]
+        },
+        {
+            "name": "playing",
+            "values": ["charades", "pictionary", "ludo", "sudoku", "badminton", "soccer", "carroms", "chess", "taboo", "password \ngame", "sequence"]
+        },
+        {
+            "name": "painting",
+            "values": ["water\ncolors", "oil\npainting", "sketching", "pencil \ndrawing", "crayons", "fingerprint\nart", "qtip\nart", "thread\npainting"]
+        },
+        {
+            "name": "gardening",
+            "values": ["plant", "pluck", "clean", "sow \n seeds", "harvest", "watering", "indoor \n plants", "outdoor \n plants", "soiling"]
+        },
+        {
+            "name": "meditating",
+            "values": ["spiritual\nmeditation", "focused\n meditation", "mindfulness\n meditation", "mantra\n meditation", "zen\n meditation"]
+        },
+        {
+            "name": "dance",
+            "values": ["contemporary\ndance", "ballet", "jazz\ndance", "tap", "hip-hop", "ballroom", "lyrical", "highland\ndance", "line\ndance", "irish\ndance"]
+        },
+        {
+            "name": "sing",
+            "values": ["pop\nsinging", "opera", "rock\nsinging", "jazz\nsinging", "scat\nsinging", "choral\nsinging"]
+        },
+          {
+            "name": "fun",
+            "values": ["crafts", "DIY\nplanter", "DIY\nnecklace", "DIY\nbracelet", "Thumbprint\nfamilytree", "Stone\npainting", "Tie\nDie"]
+        },
+        {
+            "name": "listen",
+            "values": ["water\nsounds", "music", "nature\nsounds", "white\nnoise", "guided\nmeditation", "water\ndrops", "ticking\nclock"]
+        }
+    ]
+};
